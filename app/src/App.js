bfd44c4e0c036d9a5961c7af277f7eed485edcbd
xavier-lai <xavier.lai@c-ways.com>
2022-10-15 22:34:37 +0200

    Main app

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css'
+import './css/Banner.css'
+import './css/NavBar.css'
+import './css/Skills.css'
+
+import { NavBar } from './components/NavBar'
+import { Banner } from './components/Banner'
+import { Skills } from './components/Skills'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <NavBar></NavBar>
+      <Banner></Banner>
+      <Skills></Skills>
+    </div >
   );
 }
 
