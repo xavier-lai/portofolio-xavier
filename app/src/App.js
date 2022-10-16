209fcaea71e032413e8d1174678d3b642fb0db64
xavier-lai <xavier.lai@c-ways.com>
2022-10-16 17:23:22 +0200

    Banner css animate

--- app/src/App.js
+++ app/src/App.js
@@ -2,18 +2,24 @@ import './css/App.css'
 import './css/Banner.css'
 import './css/NavBar.css'
 import './css/Skills.css'
+import './css/Projects.css'
+import './css/Contact.css'
 
 import { NavBar } from './components/NavBar'
 import { Banner } from './components/Banner'
 import { Skills } from './components/Skills'
+import { Projects } from './components/Projects'
+import { Contact } from './components/Contact'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
-      <Banner></Banner>
-      <Skills></Skills>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
     </div >
   );
 }
