1ef773a67287bd229e2c4c51da438779f9be4ff2
xavier-lai <xavier.lai@c-ways.com>
2022-10-16 22:58:44 +0200

    better navbar

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import linkedinIcon from '../assets/img/linkedin-icon.svg';
+import resumeIcon from '../assets/img/resume-icon.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
     BrowserRouter as Router
@@ -50,9 +49,10 @@ export const NavBar = () => {
                         </Nav>
                         <span className="navbar-text">
                             <div className="social-icon">
-                                <a href="#"><img src={navIcon1} alt="" /></a>
-                                <a href="#"><img src={navIcon2} alt="" /></a>
-                                <a href="#"><img src={navIcon3} alt="" /></a>
+                                <a href="https://www.linkedin.com/in/xavier-lai-85a861131/" target="_blank" rel="noreferrer"><img src={linkedinIcon} alt="My linkedin" /></a>
+                            </div>
+                            <div className="resume-icon">
+                                <a href="#" target="_blank" rel="noreferrer" ><img src={resumeIcon} alt="My resume" /></a>
                             </div>
                             <HashLink to='#connect'>
                                 <button className="vvd"><span>Let’s Connect</span></button>
