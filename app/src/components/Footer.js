de9232a30d864a2672b4f340542db143386f5bb1
xavier-lai <xavier.lai@c-ways.com>
2022-10-16 23:04:01 +0200

    Footer like navbar

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,9 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap"
 import { MailchimpForm } from "./MailChimp"
 import logo from "../assets/img/logo.svg"
-import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import linkedinIcon from '../assets/img/linkedin-icon.svg'
+import resumeIcon from '../assets/img/resume-icon.svg'
 
 
 export const Footer = () => {
@@ -17,9 +16,10 @@ export const Footer = () => {
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/xavier-lai-85a861131/"><img src={navIcon1} alt="Linkedin" /></a>
-                            <a href="https://www.linkedin.com/in/xavier-lai-85a861131/"><img src={navIcon2} alt="Github" /></a>
-                            <a href="https://www.linkedin.com/in/xavier-lai-85a861131/"><img src={navIcon3} alt="Twitter" /></a>
+                            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/xavier-lai-85a861131/"><img src={linkedinIcon} alt="Linkedin" /></a>
+                        </div>
+                        <div className="resume-icon">
+                            <a target="_blank" rel="noreferrer" href="#"><img src={resumeIcon} alt="Resume" /></a>
                         </div>
                     </Col>
                     <p>CopyRight 2022. All Rights Reserved</p>
