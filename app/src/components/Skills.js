a84c248c908bc47d7599016f8356c6efa2e8444b
xavier-lai <xavier.lai@c-ways.com>
2022-11-01 23:15:59 +0100

    Skills advanced

--- app/src/components/Skills.js
+++ app/src/components/Skills.js
@@ -1,14 +1,53 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import { Container, Row, Col } from "react-bootstrap";
+import { useState } from 'react';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
 import colorSharp from "../assets/img/color-sharp.png";
+import { VennDiagram, VennSeries, VennArc, VennLabel } from "reaviz"
 
 import masterDegreePdf from "../assets/pdf/master_degree_xavier_lai.pdf";
 
 export const Skills = () => {
+
+    const [skillType, setSkillType] = useState("Hard");
+    const updateSkillType = (newSkillType) => {
+        setSkillType(newSkillType)
+    }
+
+    const getChartData = (skillType) => {
+        if (skillType === "Hard") {
+            return [
+                { key: ["Data engineering"], data: 90 },
+                { key: ["Data analysis"], data: 70 },
+                { key: ["Front end"], data: 20 },
+                { key: ["Statistics"], data: 65 },
+                { key: ["Machine learning"], data: 70 },
+                { key: ["Ops"], data: 75 },
+                { key: ["Programming"], data: 95 },
+                { key: ["Machine learning", "Statistics"], data: 5 },
+                { key: ["Data engineering", "Ops"], data: 3 },
+                { key: ["Data analysis", "Data engineering"], data: 5 },
+                { key: ["Data analysis", "Machine learning"], data: 5 },
+                { key: ["Data analysis", "Statistics"], data: 1 },
+                { key: ["Ops", "Machine learning"], data: 1 },
+                { key: ["Data engineering", "Machine learning"], data: 5 },
+                { key: ["Data engineering", "Ops", "Machine learning"], data: 3 },
+                { key: ["Ops", "Programming"], data: 10 },
+                { key: ["Front end", "Programming"], data: 4 },
+                { key: ["Programming", "Data engineering"], data: 10 },
+                { key: ["Programming", "Data engineering", "Ops", "Machine learning"], data: 3 }
+            ]
+        } else {
+            return [
+                { key: ["Management"], data: 50 },
+                { key: ["Agility"], data: 85 },
+            ]
+        }
+
+    }
+
     const responsive = {
         superLargeDesktop: {
             // the naming can be any, depends on you.
@@ -41,31 +80,56 @@ export const Skills = () => {
                 <Row>
                     <Col>
                         <div className="skill-bx">
-                            <h2>Skills</h2>
-                            <p><a href={masterDegreePdf} target="blank">Master’s degree</a> with honours in applied mathematics, data science, digisport option</p>
-                            <Carousel responsive={responsive}>
-                                <div className="item">
-                                    <img src={meter1} alt="Image1" />
-                                    <h5>Python</h5>
+                            <Row>
+                                <Col sm={3}></Col>
+                                <Col sm={6}>
+                                    <h2>{skillType} Skills</h2>
+                                </Col >
+                                <Col sm={3}>
+                                    <DropdownButton
+                                        id="dropdown-skill-button"
+                                        title="Skills type"
+                                        className="dropdown-skill"
+                                    >
+                                        <Dropdown.Item active onClick={() => updateSkillType("Hard")}>Hard skills</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => updateSkillType("Soft")}>Soft skills</Dropdown.Item>
+                                    </DropdownButton >
+                                </Col>
+                            </Row>
+                            <Row>
+                                <p><a href={masterDegreePdf} target="blank">Master’s degree</a> with honours in applied mathematics, data science, digisport option</p>
+                                <div style={{ width: '100vw', height: '80vh' }}>
+                                    <VennDiagram
+                                        type="venn"
+                                        disabled={false}
+                                        data={getChartData(skillType)}
+                                        series={
+                                            <VennSeries
+                                                colorScheme={['#2d60e8']}
+                                                arc={
+                                                    <VennArc
+                                                        stroke={(_data, _index, active, hovered) => {
+                                                            if (hovered) {
+                                                                return "#d785ce";
+                                                            } else if (active) {
+                                                                return '#d785ce';
+                                                            }
+                                                            return 'blue';
+                                                        }}
+                                                    />
+                                                }
+                                                label={
+                                                    <VennLabel active={true} showAll={false} wrap={true} fontFamily="Centra" animated="false" />
+                                                }
+                                            />}
+                                    />
                                 </div>
-                                <div className="item">
-                                    <img src={meter2} alt="Image2" />
-                                    <h5>SQL</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={meter3} alt="Image3" />
-                                    <h5>Machine Learning</h5>
-                                </div>
-                                <div className="item">
-                                    <img src={meter1} alt="Image4" />
-                                    <h5>GCP</h5>
-                                </div>
-                            </Carousel>
+                            </Row>
                         </div>
                     </Col>
                 </Row>
             </Container>
             <img className="background-image-left" src={colorSharp} alt="BG Img" />
-        </section>
+        </section >
     )
 }
