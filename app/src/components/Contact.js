2720f6041c1e849db36c5eee09d011a6a10b3d0b
xavier-lai <xavier.lai@c-ways.com>
2022-12-25 16:37:29 +0100

    send email works

--- app/src/components/Contact.js
+++ app/src/components/Contact.js
@@ -1,46 +1,40 @@
-import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Contact = () => {
-    const formInitialDetails = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        message: ''
-    }
-    const [formDetails, setFormDetails] = useState(formInitialDetails);
-    const [buttonText, setButtonText] = useState('Send');
-    const [status, setStatus] = useState({});
 
-    const onFormUpdate = (category, value) => {
-        setFormDetails({
-            ...formDetails,
-            [category]: value
-        })
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setButtonText("Sending...");
-        let response = await fetch("http://localhost:5000/contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: JSON.stringify(formDetails),
-        });
-        setButtonText("Send");
-        let result = await response.json();
-        setFormDetails(formInitialDetails);
-        if (result.code === 200) {
-            setStatus({ succes: true, message: 'Message sent successfully' });
-        } else {
-            setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
+    const showPopUpMessage = (status) => {
+        if (status === "success") {
+            toast.success('Message sent successfully !', {
+                position: toast.POSITION.TOP_RIGHT
+            });
         }
+        else if (status === "error") {
+            toast.error('Something went wrong, please email me at xavier.lai.pro@gmail.com', {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        }
+    };
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        console.log(e.target);
+
+        emailjs.sendForm('xavier_pro_gmail', 'contact_form_portofolio', e.target, "P_BlK1js0e9LY9tpm")
+            .then((result) => {
+                showPopUpMessage("success");
+            }, (error) => {
+                showPopUpMessage("error");
+                console.log(error.text);
+            });
+
+        e.target.reset();
     };
 
     return (
@@ -59,30 +53,25 @@ export const Contact = () => {
                             {({ isVisible }) =>
                                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                                     <h2>Get In Touch</h2>
-                                    <form onSubmit={handleSubmit}>
+                                    <form onSubmit={sendEmail}>
                                         <Row>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                                                <input type="text" name="firstname" placeholder="First Name" required />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                                                <input type="text" name="lastname" placeholder="Last Name" required />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                                                <input type="email" name="email" placeholder="Email Address" required />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                                                <input type="tel" name="phone" placeholder="Phone No." />
                                             </Col>
                                             <Col size={12} className="px-1">
-                                                <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                                                <button type="submit"><span>{buttonText}</span></button>
+                                                <textarea rows="6" name="message" placeholder="Message (French/English)" required></textarea>
+                                                <button type="submit"><span>Send</span></button>
                                             </Col>
-                                            {
-                                                status.message &&
-                                                <Col>
-                                                    <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                                                </Col>
-                                            }
+                                            <ToastContainer />
                                         </Row>
                                     </form>
                                 </div>}
