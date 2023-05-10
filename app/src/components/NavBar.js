aed91c711349c110c7cc4c34658c59964f2af612
Xavier Lai <109803257+xavier-lai@users.noreply.github.com>
2023-05-10 11:45:47 +0200

    Dev (#6)
    * init commit
    
    * package and img
    
    * remove ReadMe inside app
    
    * App css in css/
    
    * remove index css
    
    * Banner
    
    * NavBar
    
    * Skills section
    
    * Main app
    
    * readME
    
    * update ReadMe
    
    * Projects
    
    * Contact
    
    * Banner css animate
    
    * package update
    
    * Mailchimp
    
    * Newsletter component
    
    * Footer
    
    * Animate css begin
    
    * final package
    
    * Better banner
    
    * Contact wrapped Tracked
    
    * Project Wrapped
    
    * Route Navbar
    
    * better navbar
    
    * Footer like navbar
    
    * icon assets
    
    * assets
    
    * Use better assets
    
    * Banner tick
    
    * main bg
    
    * new background
    
    * add prefix text
    
    * major description
    
    * hover css on let's connect button
    
    * typo
    
    * first tab of projects
    
    * Description projects
    
    * Description skills
    
    * better target href
    
    * remove border css tab pill
    
    * better hover on link css
    
    * sport projects cards img
    
    * All projects in place
    
    * better css
    
    * anchor
    
    * color
    
    * don't need newsletter component
    
    * Skills advanced
    
    * fix variable
    
    * skills section almost done
    
    * Update
    
    * Project modal done
    
    * better header modal
    
    * temp pdf
    
    * deploy script
    
    * test
    
    * resume update
    
    * change resume
    
    * better css
    
    * resume pdf
    
    * Add project date in modal contetn
    
    * projects modal
    
    * css modal
    
    * save
    
    * c-flash project
    
    * Teacher project
    
    * nextrends project
    
    * srfc image bg
    
    * better nextrends jpg
    
    * lbm project description
    
    * send email works
    
    * use env variable
    
    * Ci-Cd
    
    * tst env variable debug
    
    * update packagelock.json
    
    * debug ci-cd
    
    * workflow in app
    
    * test ci-cd
    
    * install animate css
    
    * install react on screen
    
    * linter
    
    * deploy debug
    
    * deploy ci-cd
    
    * i thnik it's working
    
    * add github token
    
    * u^date ci-cd
    
    * remove useless code
    
    * email in env variable
    
    * app version
    
    * sort import
    
    * remove server.js
    
    * update app name
    
    * update readme
    
    * other project description done
    
    * avancée
    
    * sport projects description beginning
    
    * skiing project card
    
    * Sport project done
    
    * typo
    
    * better see-more button css
    
    * add redirect to home page when click on logo

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -38,7 +38,7 @@ export const NavBar = () => {
             <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
                 <Container>
                     <Navbar.Brand href="/">
-                        <img src={logo} alt="Logo" />
+                        <a href="https://xavier-lai.github.io/portofolio-xavier/"><img src={logo} alt="Logo" /></a>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav">
                         <span className="navbar-toggler-icon"></span>
